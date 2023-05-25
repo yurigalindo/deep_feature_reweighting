bfaa5d5858797a8dfb79c060cd4956dc32cccecc
yurigalindo <yurioliveiragalindo@gmail.com>
2023-05-25 15:05:43 -0300

    feature: added linearly increasing temperature

--- train_classifier.py
+++ train_classifier.py
@@ -170,6 +170,8 @@ logger.flush()
 
 # Train loop
 for epoch in range(args.num_epochs):
+    if epoch>0:
+        T = epoch/5
     model.train()
     loss_meter = AverageMeter()
     acc_groups = {g_idx : AverageMeter() for g_idx in range(trainset.n_groups)}
@@ -183,13 +185,13 @@ for epoch in range(args.num_epochs):
             y = p
 
         optimizer.zero_grad()
-        logits = model(x)
+        logits = model(x)/T
         if args.multitask:
             logits, logits_place = logits
-            loss = criterion(logits, y) + criterion(logits_place, p)
+            loss = criterion(logits, y)*T + criterion(logits_place, p)*T
             update_dict(acc_place_groups, p, g, logits_place)
         else:
-            loss = criterion(logits, y)
+            loss = criterion(logits, y)*T
         loss.backward()
         optimizer.step()
         
